bfd7b66b48d31428088537744b629d854586ce12
NinjaNube008 <logan.houston4509@gmail.com>
2018-06-09 01:29:36 -0500

    GOTH

--- discordbot.py
+++ discordbot.py
@@ -33,8 +33,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    time = datetime.datetime.now(pytz.timezone("US/Central"))  # Gets time when the message was sent
     if message.content.upper().startswith("$TIME"):
-        time = datetime.datetime.now(pytz.timezone("US/Central"))  # Gets time when the message was sent
         hours = 19 - time.hour - 1  # Defines Hour
         if hours < 0:
             hours = 19 + 4 - time.hour % 19  # Redefines hour if hour is less than 0 (past 7pm cdt)
@@ -73,13 +73,13 @@ async def on_message(message):
         args.append("end")  # Adds 'end' to the end of the list to prevent errors
         embed = discord.Embed(title="Help", color=0x15f3ff)
         if args[1] == "commands":  # if '$help commands'
-            embed.add_field(name="StreamBot help commands", value="StreamBot Commands start with $.\nCommands are NOT case sensitive\nExisting commands are:\n\t$time\n\t$help (accepts command as parameter)\n\t$gif (accepts list as a parameter)")
+            embed.add_field(name="StreamBot Help commands", value="StreamBot Commands start with $.\nCommands are NOT case sensitive\nExisting commands are:\n\t$time\n\t$help (accepts command as parameter)\n\t$gif (accepts list as a parameter)")
         elif args[1] == "time".upper() or args[1] == "$time":  # if '$help time' or '$help $time' entered
-            embed.add_field(name="StreamBot help time", value="Time shows you the time until the next livestream from NinjaNube Gaming.\nThis command is NOT case sensitive\nThis command has no parameters")
+            embed.add_field(name="StreamBot Help time", value="Time shows you the time until the next livestream from NinjaNube Gaming.\nThis command is NOT case sensitive\nThis command has no parameters")
         elif args[1] == "gif" or args[1] == "$gif":  # if '$help gif' or '$help $gif' entered
-            embed.add_field(name="StreamBot help gif", value="Gif sends a random gif\nThis command is NOT case sensitive\nParameters: list\n\tList lists the links to the chosen gifs")
+            embed.add_field(name="StreamBot Help gif", value="Gif sends a random gif\nThis command is NOT case sensitive\nParameters: list\n\tList lists the links to the chosen gifs")
         else:  # if '$help' entered
-            embed.add_field(name="StreamBot help", value="StreamBot commands start with $ to avoid accidental calls from other bot.\nTry \'$help $time\' or \'$help commands\'")
+            embed.add_field(name="StreamBot Help", value="StreamBot commands start with $ to avoid accidental calls from other bot.\nTry \'$help $time\' or \'$help commands\'")
         await client.send_message(message.channel, embed=embed)
 
     elif message.content.startswith("$"):
@@ -88,8 +88,7 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
 
     elif time.hour % 1 == 0 and time.minute % 5 == 0:
-        time = datetime.datetime.now(pytz.timezone("US/Central"))
-        embed = discord.Embed(title="Gif of the Hour", color=0x059789)
+        embed = discord.Embed(title="GIF of the Hour", color=0x059789)
         embed.set_image(url=random.choice(urls))  # Displays gif
         # await client.send_message(discord.Object("454027523270115358"), embed=embed)
         # await client.send_message(discord.Object("454369180951511051"), embed=embed)
