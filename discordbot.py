088be62bb862bfe85e819d95bf725dc9b8e40ea4
NinjaNube008 <logan.houston4509@gmail.com>
2018-06-08 22:33:22 -0500

    try $invite

--- discordbot.py
+++ discordbot.py
@@ -70,7 +70,7 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
 
     elif message.content.upper().startswith("$INVITE"):
-        invite = discord.Client.create_invite(destination=message.server, max_age=0, max_uses=10, unique=False)
+        invite = discord.Client.create_invite(self, destination=message.server, max_age=0, max_uses=10, unique=False)
         embed = discord.Embed(title="INVITE LINK", color=0x8b8fa1)  # 8b8fa1
         embed.add_field(name="This is the server\'s invite link", value="Expire Time: No Limit\nMax Uses: 10\nURL: " + str(invite))
         await client.send_message(message.channel, embed=embed)
