ef878aea8a617607b430787b26663b833b5e0602
NinjaNube008 <logan.houston4509@gmail.com>
2018-06-09 00:26:35 -0500

    added swear.txt

--- discordbot.py
+++ discordbot.py
@@ -18,7 +18,7 @@ urls = ["https://media.giphy.com/media/9pCESofHVLvcA/giphy.gif",
         "https://media.giphy.com/media/5UH4ZEelyBwwIW8E2k/giphy.gif",
         "https://media.giphy.com/media/26Ff0l4TARWjMTSKs/giphy.gif"]
 
-with open("swear.txt") as fp:
+with open("swear.txt", "rb") as fp:
     filter = fp.readlines()
 filter = [x.strip() for x in filter]
 
