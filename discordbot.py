b9329526f321afa9845c37def9e92cc2a8a68007
NinjaNube008 <logan.houston4509@gmail.com>
2018-06-08 21:19:49 -0500

    try $gif list

--- discordbot.py
+++ discordbot.py
@@ -56,29 +56,36 @@ async def on_message(message):
         embed.add_field(name="NinjaNube_Gaming is livestreaming!", value="Visit Twitch: http://twitch.tv/ninjanube_gaming\nVisit Mixer: http://mixer.com/NinjaNube_Gaming\nVisit YouTube: http://www.youtube.com/channel/UCS4NeZkk0B8gkK5uGKxIo2w")
         await client.send_message(message.channel, embed=embed)
 
-    elif message.content.upper().startswith("$HELP"):
-        args = message.content.split(" ")  # Makes parameter list
-        args.append("end")  # Adds 'end' to the end of the list to prevent errors
-        embed = discord.Embed(title="Help", color=0x15f3ff)
-        if args[1] == "commands":
-            embed.add_field(name="StreamBot help commands", value="StreamBot Commands start with $.\nCommands are NOT case sensitive\nExisting commands are:\n\t$time\n\t$help (accepts parameter)")
-        elif args[1] == "time" or args[1] == "$time":
-            embed.add_field(name="StreamBot help time", value="Time shows you the time until the next livestream from NinjaNube Gaming.\nThis command is NOT case sensitive\nThis command has no parameters")
-        else:
-            embed.add_field(name="StreamBot help", value="StreamBot commands start with $ to avoid accidental calls from other bot.\nWe don\'t use spaces or parameters in any command other than this one.\nTry \'$help $time\' or \'$help commands\'")
-        await client.send_message(message.channel, embed=embed)
-
     elif message.content.upper().startswith("$GIF"):
+        args = message.content.split(" ")  # Makes parameter list
+        args.append("end")
         gifNum = len(urls)
         gif = randint(0, gifNum)
         embed = discord.Embed(title="GIF", color=0x059789)
-        embed.add_field(name="Here\'s a random gif!", value="You got gif " + str(gif + 1) + "/" + str(gifNum))
-        embed.set_image(url=urls[gif])
+        if args[1] == "LIST".upper():
+            embed.add_field(name="List of gif links that can be sent", value="\n\t".join(urls))
+        else:
+            embed.add_field(name="Here\'s a random gif!", value="You got gif " + str(gif + 1) + "/" + str(gifNum))
+            embed.set_image(url=urls[gif])
         await client.send_message(message.channel, embed=embed)
 
     elif message.content.upper().startswith("$LIST"):
         embed = discord.Embed(title="FILTER", color=0x8b8fa1)  # 8b8fa1
         embed.add_field(name="Words you can\'t say", value="\n".join(filter[0:]).lower())
+        await client.send_message(message.channel, embed=embed)
+
+    elif message.content.upper().startswith("$HELP"):
+        args = message.content.split(" ")  # Makes parameter list
+        args.append("end")  # Adds 'end' to the end of the list to prevent errors
+        embed = discord.Embed(title="Help", color=0x15f3ff)
+        if args[1] == "COMMANDS".upper():
+            embed.add_field(name="StreamBot help commands", value="StreamBot Commands start with $.\nCommands are NOT case sensitive\nExisting commands are:\n\t$time\n\t$help (accepts command as parameter)\n\t$gif (accepts list as a parameter)")
+        elif args[1] == "TIME".upper() or args[1] == "$TIME".upper():
+            embed.add_field(name="StreamBot help time", value="Time shows you the time until the next livestream from NinjaNube Gaming.\nThis command is NOT case sensitive\nThis command has no parameters")
+        elif args[1] == "GIF".upper() or args[1] == "$GIF".upper():
+            embed.add_field(name="StreamBot help gif", value="Gif sends a random gif\nThis command is NOT case sensitive\nParameters: list\n\tList lists the links to the chosen gifs")
+        else:
+            embed.add_field(name="StreamBot help", value="StreamBot commands start with $ to avoid accidental calls from other bot.\nTry \'$help $time\' or \'$help commands\'")
         await client.send_message(message.channel, embed=embed)
 
     elif message.content.startswith("$"):
