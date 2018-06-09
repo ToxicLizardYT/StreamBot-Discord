2451af9da2c62b92421f9fd6175baa150f2ec771
NinjaNube008 <logan.houston4509@gmail.com>
2018-06-08 19:39:46 -0500

    try $gif

--- discordbot.py
+++ discordbot.py
@@ -64,7 +64,7 @@ async def on_message(message):
         gif = randint(1, gifNum)
         embed = discord.Embed(title="GIF", color=0x749dee)
         embed.add_field(name="Here\'s a random gif!", value="You got gif " + str(gif) + "/" + str(gifNum))
-        embed.set_image(url=str(gif) + ".gif")
+        embed.set_image(url="./gifs/" + str(gif) + ".gif")
         await client.send_message(message.channel, embed=embed)
 
     elif message.content.upper().startswith("$LIST"):
