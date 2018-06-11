cd643393050474e852595a801465c36041ac233d
NinjaNube008 <logan.houston4509@gmail.com>
2018-06-10 19:38:11 -0500

    Better commenting

--- discordbot.py
+++ discordbot.py
@@ -11,27 +11,30 @@ client = Bot(command_prefix="$")
 
 RapGod_id = "324332386219327488"
 
-urls = [
-        "https://media.giphy.com/media/9pCESofHVLvcA/giphy.gif",
-        "https://media.giphy.com/media/iAQFuaZSrDnBuVCdZO/giphy.gif",
-        "https://media.discordapp.net/attachments/291284109232308226/454794855021805578/raw.gif",
-        "https://media.giphy.com/media/5Yl6uqC0YQHejTKi7Q/giphy.gif",
-        "https://media.giphy.com/media/5UH4ZEelyBwwIW8E2k/giphy.gif",
-        "https://media.giphy.com/media/26Ff0l4TARWjMTSKs/giphy.gif",
-        "https://media.giphy.com/media/o7BebTgj44jmw/giphy.gif",
-        "https://media.giphy.com/media/l396O8kn1qbPcb4ha/giphy.gif",
-        "https://media.giphy.com/media/9r3mtPn0arLJ227VjZ/giphy.gif",
-        "https://media.giphy.com/media/xT0xeP5zQrtRH3v8KA/giphy.gif",
-        "https://media.giphy.com/media/103t71VKmtY1UY/giphy.gif",
-        "https://media.giphy.com/media/3osBLqTeJNPToFnh6M/giphy.gif"
-    ]
+urls = [  # List of gif urls
+    "https://media.giphy.com/media/9pCESofHVLvcA/giphy.gif",
+    "https://media.giphy.com/media/iAQFuaZSrDnBuVCdZO/giphy.gif",
+    "https://media.discordapp.net/attachments/291284109232308226/454794855021805578/raw.gif",
+    "https://media.giphy.com/media/5Yl6uqC0YQHejTKi7Q/giphy.gif",
+    "https://media.giphy.com/media/5UH4ZEelyBwwIW8E2k/giphy.gif",
+    "https://media.giphy.com/media/26Ff0l4TARWjMTSKs/giphy.gif",
+    "https://media.giphy.com/media/o7BebTgj44jmw/giphy.gif",
+    "https://media.giphy.com/media/l396O8kn1qbPcb4ha/giphy.gif",
+    "https://media.giphy.com/media/9r3mtPn0arLJ227VjZ/giphy.gif",
+    "https://media.giphy.com/media/xT0xeP5zQrtRH3v8KA/giphy.gif",
+    "https://media.giphy.com/media/103t71VKmtY1UY/giphy.gif",
+    "https://media.giphy.com/media/3osBLqTeJNPToFnh6M/giphy.gif",
+    "https://media.giphy.com/media/hiFfdSwE5oF7q/giphy.gif",
+    "https://media.giphy.com/media/xlGYf1RUbYYes/giphy.gif",
+    "https://media.giphy.com/media/SL9eV3oQYJUtj97WlA/giphy.gif"
+]
 
 with open("swear.txt", "rt") as fp:
     filter = fp.readlines()
 filter = [x.strip() for x in filter]
 
 
-async def background_loop():
+async def background_loop():  # Gif of the hour
     while True:
         Time = datetime.datetime.now(pytz.timezone("US/Central"))  # Gets time when the message was sent
         if Time.minute == 0:
@@ -44,7 +47,7 @@ async def background_loop():
 
 
 @client.event
-async def on_ready():
+async def on_ready():  # When bot is up
     embed = discord.Embed(title="Updated!", color=0xBA00AA)
     await client.send_message(discord.Object("454027523270115358"), embed=embed)
     await client.send_message(discord.Object("454369180951511051"), embed=embed)
@@ -52,7 +55,7 @@ async def on_ready():
 
 
 @client.event
-async def on_member_join(member):
+async def on_member_join(member):  # When a member joins the server
     embed = discord.Embed(name="Welcome!", color=0xff0080)
     embed.add_field(name="Welcome to the server!", value="try \'$help commands\' for a list of commands")
     if str(member.server.id) == "454393067974426634":
@@ -64,9 +67,9 @@ async def on_member_join(member):
 
 
 @client.event
-async def on_message(message):
+async def on_message(message):  # When message is sent
     Time = datetime.datetime.now(pytz.timezone("US/Central"))  # Gets time when the message was sent
-    if message.content.upper().startswith("$TIME"):
+    if message.content.upper().startswith("$TIME"):  # Gets time until 7pm CDT
         hours = 19 - Time.hour - 1  # Defines Hour
         if hours < 0:
             hours = 19 + 4 - Time.hour % 19  # Redefines hour if hour is less than 0 (past 7pm cdt)
@@ -80,17 +83,17 @@ async def on_message(message):
             embed.add_field(name="Time until next stream:", value="{} hour(s)\n{} minute(s)!".format(hours, minutes))   # Show Normal Time
         await client.send_message(message.channel, embed=embed)
 
-    elif message.author.id == "159985870458322944" and message.content.startswith("Hey @everyone!") or message.content.upper().startswith("$LIVE"):
-        try:
-            await client.delete_message(message)  # Detects message from Mee6 bot announcing live and replaces with StreamBot live announcement
-        except discord.errors.Forbidden:
-            await client.send_message(message.channel, "***Missing Permissions***")
-        await client.send_message(message.channel, "@everyone")
-        embed = discord.Embed(title="New Stream", color=0xff1ff3)
-        embed.add_field(name="NinjaNube_Gaming is livestreaming!", value="Visit Twitch: http://twitch.tv/ninjanube_gaming\nVisit Mixer: http://mixer.com/NinjaNube_Gaming\nVisit YouTube: http://www.youtube.com/channel/UCS4NeZkk0B8gkK5uGKxIo2w")
-        await client.send_message(message.channel, embed=embed)
+    # elif message.author.id == "159985870458322944" and message.content.startswith("Hey @everyone!") or message.content.upper().startswith("$LIVE"):
+    #     try:
+    #         await client.delete_message(message)  # Detects message from Mee6 bot announcing live and replaces with StreamBot live announcement
+    #     except discord.errors.Forbidden:
+    #         await client.send_message(message.channel, "***Missing Permissions***")
+    #     await client.send_message(message.channel, "@everyone")
+    #     embed = discord.Embed(title="New Stream", color=0xff1ff3)
+    #     embed.add_field(name="NinjaNube_Gaming is livestreaming!", value="Visit Twitch: http://twitch.tv/ninjanube_gaming\nVisit Mixer: http://mixer.com/NinjaNube_Gaming\nVisit YouTube: http://www.youtube.com/channel/UCS4NeZkk0B8gkK5uGKxIo2w")
+    #     await client.send_message(message.channel, embed=embed)
 
-    elif message.content.upper().startswith("$GIF"):
+    elif message.content.upper().startswith("$GIF"):  # Sends random gif from the urls list
         args = message.content.split(" ")  # Makes parameter list
         args.append("end")
         embed = discord.Embed(title="GIF", color=0x059789)
@@ -100,7 +103,7 @@ async def on_message(message):
             embed.set_image(url=random.choice(urls))  # Displays gif
         await client.send_message(message.channel, embed=embed)
 
-    elif message.content.upper().startswith("$INFO"):
+    elif message.content.upper().startswith("$INFO"):  # Gives user info
         args = message.content.split(" ")
         args.append("end")
         embed = discord.Embed(title="User Info", color=0x50affb)
@@ -115,7 +118,7 @@ async def on_message(message):
             embed.add_field(name="No member tagged", value="Please tag a member when using this function")
         await client.send_message(message.channel, embed=embed)
 
-    elif message.content.upper().startswith("$HELP"):
+    elif message.content.upper().startswith("$HELP"):  # Basic help command
         args = message.content.split(" ")  # Makes parameter list
         args.append("end")  # Adds 'end' to the end of the list to prevent errors
         embed = discord.Embed(title="Help", color=0x15f3ff)
@@ -124,7 +127,7 @@ async def on_message(message):
         elif args[1] == "time" or args[1] == "$time":  # if '$help time' or '$help $time' entered
             embed.add_field(name="StreamBot Help time", value="Time shows you the time until the next livestream from NinjaNube Gaming.\nThis command is NOT case sensitive\nThis command has no parameters")
         elif args[1] == "gif" or args[1] == "$gif":  # if '$help gif' or '$help $gif' entered
-            embed.add_field(name="StreamBot Help gif", value="Gif sends a random gif\nThis command is NOT case sensitive\nParameters: list, all\n\tList lists the links to the chosen gifs\n\tAll shows all possible gifs. Mainly used for debugging")
+            embed.add_field(name="StreamBot Help gif", value="Gif sends a random gif\nThis command is NOT case sensitive\nParameters: list\n\tList lists the links to the chosen gifs")
         elif args[1] == "info" or args[1] == "$info":  # if '$help info' or '$help $info' entered
             embed.add_field(name="StreamBot Help info", value="Info sends the tagged user's information\nThis command is NOT case sensitive\nParameters: tag a user\n\tTagging makes the command work")
         else:  # if '$help' entered
@@ -136,7 +139,7 @@ async def on_message(message):
         embed.add_field(name="That command doesn\'t exist", value="Use $help for help")
         await client.send_message(message.channel, embed=embed)
 
-    else:
+    else:  # Chat Filter
         contents = message.content.split(" ")
         for word in contents:
             if word.upper() in filter:
